92ed2c5d5ad9d8fd5c4b651c0d154ab857b7b2f6
cyrus <online_mad@yahoo.com.hk>
2018-09-08 11:31:20 +0800

    start workbox

--- sw.js
+++ sw.js
@@ -2,28 +2,42 @@ const staticAssests = [
     './',
     './styles.css',
     './app.js',
+    './fallback.json',
+    './images/fetch-dog.jpg'
 ];
 
 
-self.addEventListener('install',async event=>{
-    const cache = await caches.open('news-static');
-    cache.addAll(staticAssests)
-})
+// self.addEventListener('install',async event=>{
+//     const cache = await caches.open('news-static');
+//     cache.addAll(staticAssests)
+// })
 
-self.addEventListener('fetch',event=>{
-    const req = event.request;
-    const url = new URL(request.url);
+// self.addEventListener('fetch',event=>{
+//     const req = event.request;
+//     const url = new URL(req.url);
     
-    if (url.origin === location.origin) {
-        event.respondWith(cacheFirst(req));
-    }else{
-        event.respondWith(networkFirst(req));
-    }
-    console.log('fetch')
-})
+//     if (url.origin === location.origin) {
+//         event.respondWith(cacheFirst(req));
+//     }else{
+//         event.respondWith(networkFirst(req));
+//     }
+//     console.log('fetch')
+// })
 
-async function cacheFirst(req){
-    const cachedResponse = await caches.match(req);
-    return cachedResponse || fetch(req);
-}
+// async function cacheFirst(req){
+//     const cachedResponse = await caches.match(req);
+//     return cachedResponse || fetch(req);
+// }
 
+// async function networkFirst(req){
+//     const cache = await caches.open('news-dynamic');
+
+//     try {
+//         const res = await fetch(req);
+//         cache.put(req,res.clone());
+//         return res;
+//     } catch (error) {
+//         const cachedResponse = await cache.match(req);
+//         return cachedResponse || await caches.match('./fallback.json');
+//     }
+// }
